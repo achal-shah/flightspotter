30892e58d28279113b19be70c5d2406204e16f26
Achal Shah <achal.shah@hotmail.com>
2025-12-24 18:27:35 -0800

    To be able to run in the cloud.

--- FlightSpotter.Web/Program.cs
+++ FlightSpotter.Web/Program.cs
@@ -1,3 +1,4 @@
+using Azure.Identity;
 using FlightSpotter.Web.Services;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authentication.OpenIdConnect;
@@ -5,6 +6,13 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.IdentityModel.Validators;
 
 var builder = WebApplication.CreateBuilder(args);
+var cfg = builder.Configuration;
+var keyVaultUrl = builder.Configuration["KeyVault:Url"];
+
+if (!string.IsNullOrEmpty(keyVaultUrl))
+{
+    builder.Configuration.AddAzureKeyVault( new Uri(keyVaultUrl), new DefaultAzureCredential());
+}
 
 // Authentication: use OpenID Connect (Microsoft identity platform v2.0) with cookie auth as the local scheme.
 builder.Services.AddAuthentication(options =>
@@ -15,7 +23,6 @@ builder.Services.AddAuthentication(options =>
     .AddCookie()
     .AddOpenIdConnect(OpenIdConnectDefaults.AuthenticationScheme, options =>
     {
-        var cfg = builder.Configuration;
         // Use the v2.0 common endpoint to allow both work/school and personal Microsoft accounts
         options.Authority = "https://login.microsoftonline.com/common/v2.0";
         options.ClientId = cfg["AzureAd:ClientId"];
@@ -28,7 +35,6 @@ builder.Services.AddAuthentication(options =>
         options.Scope.Add("profile");
         options.TokenValidationParameters.IssuerValidator =
             AadIssuerValidator.GetAadIssuerValidator(options.Authority).Validate;
-
     });
 
 // Require authentication by default for all requests (only authenticated MSA users can access)
